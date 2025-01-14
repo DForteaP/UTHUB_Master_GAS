af3958be37719dbf3093f461cc722f5c3d7e9515
DForteaP <dforteap@gmail.com>
2025-01-14 11:06:20 +0100

    FirstClass

---
+++ Source/UTHUB_GAS/UTHUB_GAS.Build.cs
@@ -0,0 +1,13 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class UTHUB_GAS : ModuleRules
+{
+	public UTHUB_GAS(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+        PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "NavigationSystem", "AIModule", "Niagara", "EnhancedInput" });
+    }
+}
